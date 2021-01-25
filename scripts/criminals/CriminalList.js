82c90fd3d0752f961c8014f835077e981dc1c5e8
Dylan Morris <dylanrobertmorris@gmail.com>
2021-01-25 09:39:27 -0600

    successfully got criminals to display in console and on browser, moving on to css

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,1 +1,24 @@
 import { getCriminals, useCriminals } from './CriminalProvider.js'
+import { Criminal } from './Criminal.js'
+
+
+
+export const criminalList = () => {
+    
+    getCriminals()
+        .then(() => {
+            const criminalArray = useCriminals()
+            const criminalContainer = document.querySelector(".criminalsContainer")
+            let criminalHTMLRepresentation = ""
+    
+            for (const criminal of criminalArray) {
+                criminalHTMLRepresentation += Criminal(criminal)
+            }
+    
+            criminalContainer.innerHTML += `
+            <h3> Criminals <h3>
+            <article class="criminalList>
+                ${criminalHTMLRepresentation}
+            </article>`
+        })
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,19 @@
 import { useOfficers, getOfficers } from './officers/OfficerProvider.js'
+// import { useCriminals, getCriminals } from './criminals/CriminalProvider.js'
+import { criminalList } from './criminals/CriminalList.js'
+
 
 const useOfficersData = useOfficers()
 console.log(useOfficersData)
 
 const getOfficersData = getOfficers()
 console.log(getOfficersData)
+
+// const useCriminalsData = useCriminals()
+// console.log("Criminal Data:", useCriminalsData)
+
+// const getCriminalsData = getCriminals()
+// console.log(getCriminalsData)
+
+const displayCriminals = criminalList()
+console.log("display criminals", displayCriminals)

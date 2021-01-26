a2ce386cfca2f3cedbbdf3e9aa3d125efe6281dc
Dylan Morris <dylanrobertmorris@gmail.com>
2021-01-26 09:51:00 -0600

    added .join() to convictions dropdown bar in method code to remove rogue commas from the dom

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -24,7 +24,7 @@ const render = convictionsCollection => {
                 return `
                 
                 <option value="${convictionObj.id}">${conviction}</option>`
-            })
+            }).join("")
         }
         </select>
     `

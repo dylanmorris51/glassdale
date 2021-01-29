3d1729fd59b039fe9785ce7e6e333b32c874ee1e
Dylan Morris <dylanrobertmorris@gmail.com>
2021-01-29 15:01:37 -0600

    added event listener for click event on the show witness statements button. TODO: figure out which module is listening for this event and code a listener

--- scripts/witnesses/ShowWitnessButton.js
+++ scripts/witnesses/ShowWitnessButton.js
@@ -4,3 +4,12 @@ const contentTarget = document.querySelector(".witnessButton")
 export const ShowWitnessButton = () => {
     contentTarget.innerHTML = "<button id='showWitness'>Show Witness Statements</button>"
 }
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "showWitness") {
+        const customEvent = new CustomEvent("showWitnessStatements")
+        eventHub.dispatchEvent(customEvent)
+    }
+})
+
+// Add a listener to the module that is listening for this event (this is the talking module)
